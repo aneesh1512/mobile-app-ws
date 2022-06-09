a6ba44a580150fb89c0cfa289dcf972f700ddda9
aneesh1512 <aneesh15121996@gmail.com>
2022-06-09 11:19:50 +0530

    initial commit to eureka

--- src/main/java/com/webblog/app/ws/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/webblog/app/ws/mobileappws/MobileAppWsApplication.java
@@ -3,6 +3,7 @@ package com.webblog.app.ws.mobileappws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//demo
 @SpringBootApplication
 public class MobileAppWsApplication {
 
