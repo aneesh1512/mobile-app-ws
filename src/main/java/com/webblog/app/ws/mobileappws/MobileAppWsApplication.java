56ec7527eca7cab80007d118ac6f39bf0ab6d69c
aneesh1512 <aneesh15121996@gmail.com>
2022-06-10 10:27:08 +0530

    eureka client

--- src/main/java/com/webblog/app/ws/mobileappws/MobileAppWsApplication.java
+++ src/main/java/com/webblog/app/ws/mobileappws/MobileAppWsApplication.java
@@ -2,9 +2,11 @@ package com.webblog.app.ws.mobileappws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//demo
 @SpringBootApplication
+//@EnableDiscoveryClient
 public class MobileAppWsApplication {
 
 	public static void main(String[] args) {
